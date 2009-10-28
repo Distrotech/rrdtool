2bd0bef5ea3a5d5efc074621b8a6ef2368291a4d
oetiker <oetiker@a5681a0c-68f1-0310-ab6d-d61299d08faa>
2009-10-28 20:05:35 +0000

    update for 1.4
    git-svn-id: svn://svn.oetiker.ch/rrdtool/branches/1.4@1966 a5681a0c-68f1-0310-ab6d-d61299d08faa

---
+++ website/doc/build.sh
@@ -0,0 +1,25 @@
+
+#!/bin/sh
+sect=doc
+src=../../program
+. ../bin/pod2wml.sh
+
+pod2descr() {
+	pod=$1.pod
+        descr=`grep " - " $pod|head -1|sed 's/.*- //'`  
+        menu=`grep " - " $pod|head -1|sed 's/ -.*//'`     
+}
+
+
+# build probe list
+rm -f navbar.inc
+rm -f index.inc
+
+for pod in `cd $src/doc/;echo rrdtool.pod;echo rrdcached.pod;ls *.pod|egrep -v 'rrdcached.pod|tutorial|beginners|graph-old|bin_dec|rrdtool.pod|python|RRD|ruby|lua|thread'`; do
+ base=`echo $pod |sed 's,.pod,,'` 
+ echo $base
+ cat  $src/doc/$pod > $base.pod
+ pod2descr $base
+ pod2wml $base
+done
+
