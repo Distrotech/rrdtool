18245753e6530614b2ec95bcc077bc6f127da5f5
oetiker <oetiker@a5681a0c-68f1-0310-ab6d-d61299d08faa>
2011-07-03 14:00:14 +0000

    updates
    git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk@2194 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- talks/fisl12/ex/graph.sh
+++ talks/fisl12/ex/graph.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 rrdtool graphv first.png \
+   --zoom=2 \
    --title="FISL12 RRDtool Demo" \
    --start=1199999000 --end=1200001300 \
-   --lower-limit=0 \
    DEF:t=first.rrd:temperature:AVERAGE \
-   'AREA:t#880011:Temperature'
+   'AREA:t#ff8888:Temperature' \
+   'LINE0.5:0#880000::STACK'
